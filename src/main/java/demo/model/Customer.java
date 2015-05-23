6220da736db82f939b12c64110f7602683fec108
Nicolas Frankel <nicolas@frankel.ch>
2015-05-23 08:15:22 +0300

    Final changes (I hope)

--- src/main/java/demo/model/Customer.java
+++ src/main/java/demo/model/Customer.java
@@ -5,34 +5,34 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Reservation {
+public class Customer {
 
-    Reservation() { // jpa
+    Customer() { // jpa
     }
 
-    public Reservation(String reservationName) {
-        this.reservationName = reservationName;
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     @Id
     @GeneratedValue
     private Long id;
 
-    private String reservationName;
+    private String firstName;
+
+    private String lastName;
 
     public Long getId() {
         return id;
-    }
-
-    public String getReservationName() {
-        return reservationName;
     }
 
     @Override
     public String toString() {
         return "Reservation{" +
                 "id=" + id +
-                ", reservationName='" + reservationName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
                 '}';
     }
 
